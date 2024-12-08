f1901294e3f6af97a042dc3e3a0bb84898b3a5e6
Stefano <ste.morelli1@stud.uniroma3.it>
2024-12-08 11:34:33 +0000

    Creato il listener per Connesioni Event

---
+++ recensioni-seguite/src/main/java/asw/goodmusic/recensioniseguite/messagelistner/kafka/ConnessioniEventKafkaListener.java
@@ -0,0 +1,32 @@
+package asw.goodmusic.recensioniseguite.messagelistner.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import asw.goodmusic.common.api.DomainEvent;
+import asw.goodmusic.recensioniseguite.domain.ConnessioniEventConsumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+
+import java.util.logging.Logger;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+@Component
+public class ConnessioniEventKafkaListner {
+    @Autowired
+    private ConnessioniEventConsumer connessioniEventConsumer;
+    private final Logger logger = Logger.getLogger(this.getClass().toString());
+    @Value("${asw.kafka.channel.in1}")
+    private String channel;
+
+    @Value("${asw.kafka.groupid}")
+    private String groupId;
+    
+    @KafkaListener(topics =  "${asw.kafka.channel.in1}", groupId="${asw.kafka.groupid}")
+    public void listen(ConsumerRecord<String, DomainEvent> record) throws Exception {
+        logger.info("MESSAGE LISTENER: " + record.toString());
+        DomainEvent event = record.value();
+        connessioniEventConsumer.onEvent(event); 
+    }
+}
