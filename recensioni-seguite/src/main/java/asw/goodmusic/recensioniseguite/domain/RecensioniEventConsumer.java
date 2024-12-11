737dc953b1598fa171cc04d3cb7b72a233de0d69
AlessandroSchmitt <alessandroschmitt03@gmail.com>
2024-12-11 11:34:33 +0000

    aggiunte classi recensioni seguite

---
+++ recensioni-seguite/src/main/java/asw/goodmusic/recensioniseguite/domain/RecensioniEventConsumer.java
@@ -0,0 +1,40 @@
+package asw.goodmusic.recensioniseguite.domain;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import asw.goodmusic.common.api.DomainEvent;
+import asw.goodmusic.recensioni.api.event.RecensioneCreatedEvent;
+import asw.goodmusic.recensioni.api.event.RecensioneDeletedEvent;
+
+@Service
+public class RecensioniEventConsumer {
+	
+	@Autowired
+	private RecensioniRepository recensioniRepository;
+
+	private final Logger logger = Logger.getLogger(this.getClass().toString());
+
+    public void onEvent(DomainEvent event) {
+		if (event instanceof RecensioneCreatedEvent evt) {
+			onRecensioneCreated(evt); 
+		}else if (event instanceof RecensioneDeletedEvent evt){
+			onRecensioneDeleted(evt); 
+		} else {
+			logger.info("UNKNOWN EVENT: " + event);			
+		}
+	}
+
+	private void onRecensioneCreated(RecensioneCreatedEvent event) {
+		Recensione recensione = new Recensione(event.getId(),event.getRecensore(),event.getAlbum(),event.getArtista(),event.getGenere(), event.getSunto());
+		recensioniRepository.save(connessione);
+		logger.info("CREATED CONNESSIONE: " + recensione);
+	}
+
+	private void onRecensioneDeleted(RecensioneDeletedEvent event) {
+
+		logger.info("DELETED CONNESSIONE: ");
+	}
+}
