b65a686a99fe0ee0d4af60fce2a7798e91bc66c1
Giovanni <oligiovi2@gmail.com>
2024-12-28 18:40:23 +0000

    buona parte di kubernetes

--- start-goodmusic.sh
+++ start-goodmusic.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Script per avviare l'applicazione GoodMusic
-
-echo Running GOODMUSIC 
-
-docker run -d --hostname localhost --name asw-consul --publish 8500:8500 docker.io/hashicorp/consul
-
-./recensioni/start.sh
-./connessioni/start.sh
-./recensioni-seguite/start.sh
+./kafka/deploy-kafka.sh
 cd "$(dirname "$0")"
-sleep 4 
-
-java -Xms64m -Xmx128m -jar recensioni/build/libs/recensioni.jar &
-java -Xms64m -Xmx128m -jar connessioni/build/libs/connessioni.jar &
-java -Xms64m -Xmx128m -jar recensioni-seguite/build/libs/recensioni-seguite.jar &
-
-java -Xms64m -Xmx128m -jar api-gateway/build/libs/api-gateway.jar &
+kubectl apply -f asw-goodmusic-application-multi.yaml
--- stop-goodmusic.sh
+++ stop-goodmusic.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-echo Halting GOODMUSIC   
-
-pkill -f 'recensioni-seguite.jar'
-pkill -f 'recensioni.jar'
-pkill -f 'connessioni.jar'
-pkill -f 'api-gateway.jar'
-
-sleep 4 
-
-docker stop asw-consul 
-docker rm asw-consul 
-./recensioni/stop.sh
-./connessioni/stop.sh
-./recensioni-seguite/stop.sh
+kubectl delete namespace kafka
+kubectl delete all --all -n asw
+kubectl delete -f asw-goodmusic-application-multi.yaml
+kubectl delete namespace asw
