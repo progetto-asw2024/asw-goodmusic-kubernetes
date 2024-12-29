e73db0afa76dec8088b2541443e3cc24ddddcbbe
lorenzo <lollipopsacco@gmail.com>
2024-12-29 11:27:46 +0000

    Rimossi file inutilizzati

--- kafka/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script per arrestare Postgres con Docker Compose (v2)
-
-echo Halting Kafka...
-
-# docker compose down -v
-cd "$(dirname "$0")" 
-docker compose --profile debug down -v
