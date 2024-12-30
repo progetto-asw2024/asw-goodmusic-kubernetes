6395133e6e75ab4234af4e44dffbcda0f5872be7
Stefano <ste.morelli1@stud.uniroma3.it>
2024-12-30 12:54:05 +0000

    Inizio Pulizia

--- kafka/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script per avviare Postgres con Docker Compose (v2)
-
-echo Starting Kafka...
-
-# docker compose up -d 
-cd "$(dirname "$0")" 
-docker compose --profile debug up -d 
